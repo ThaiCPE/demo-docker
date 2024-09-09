f70d935ca0c070bcc087c4765bf208491fd138fe
IS ⚡️ <1588186+issarapong@users.noreply.github.com>
2024-09-09 09:59:27 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,34 @@
 
+#!/bin/bash
+
+# Update the apt package index
+sudo apt-get update -y
+
+# Install packages to allow apt to use a repository over HTTPS
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+# Add Docker’s official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+# Set up the stable repository
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable" -y
+
+# Update the apt package index again
+sudo apt-get update -y
+
+# Install the latest version of Docker Engine and containerd
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+# Verify that Docker Engine is installed correctly by running the hello-world image
+sudo docker run hello-world
+
+# Enable Docker to start on boot
+sudo systemctl enable docker
